718b8889512e0348f45f6c00fbca72c0bd52fa03
Lucas Moreira da Silva <91037802+Lucas27Moreira@users.noreply.github.com>
2023-11-01 10:23:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,3 @@ btn_menu.addEventListener('click',(evt)=>{
     }else{
         nav.style.display="";
     }
-    // let el = evt.target.nextElementSibling;
-    // if(el.style.display == ""||el.style.display == "none"){
-        // el.style.display="block";
-    // }else{
-        // el.style.display="none";
-        // 
-    // }
-})
-
-// let tempo = setInterval(displaymudar,1000);
-// 
-// function displaymudar(){
-    // if(window.innerWidth >= 800){
-        // nav.style.display="block";
-    // }
-    //  
-    // 
-// }
